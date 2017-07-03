858a35837e0b5492f354abab095ae677ff73cb5c
lrobinsonco <lrobinsonco@gmail.com>
2017-07-03 12:51:34 -0600

    changed background style

--- src/App.js
+++ src/App.js
@@ -7,14 +7,35 @@ import Image3 from './assets/img3.jpg'
 import Image4 from './assets/img4.jpg'
 import Image5 from './assets/img5.jpg'
 
+const image =[Image1, Image2, Image3, Image4, Image5]
+const names =["Lee", "Nara", "JU"]
 
 class App extends Component {
+  constructor(){
+    super()
+    this.state = {
+      name: "peaches",
+      image: Image2
+    }
+  }
+
+  handleClick = () => {
+    let i = Math.floor(Math.random() *3)
+    this.setState({name : names[i]})
+  }
+  
   render() {
+
+    setTimeout(() =>{
+      let i= Math.floor(Math.random() * 5)
+      this.setState({image: image[i]})
+    }, 6000)
+
     return (
       <div>
 
-        <img src={Image1} className="image"></img>
-        <h1 className="hello">Hello World</h1>
+        <img src={this.state.image} className="image"></img>
+        <h1 className="hello" onClick={this.handleClick}>Hello {this.state.name}</h1>
       </div>
     );
   }
